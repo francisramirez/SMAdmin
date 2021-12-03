98fae40c955180eb71552de4915fa76870e173a7
Francis Ricardo Ramirez De Leon <f.ramirez@simv.gob.do>
2021-12-03 12:25:10 -0400

    Adding Suppliers Dependencies.

---
+++ SMWebApp/Dependency/SupplierDependency.cs
@@ -0,0 +1,25 @@
+﻿using DataInMemory.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using UseCases.Contracts;
+using UseCases.UseCaseInterfaces;
+using UseCases.ViewUsesCasesModels.Suppliers;
+
+namespace SMWebApp.Dependency
+{
+    public static class SupplierDependency
+    {
+        public static IServiceCollection AddCategoryDependency(this IServiceCollection services)
+        {
+
+            //Repositories//
+            services.AddScoped<ISupplierRepository, SupplierRepository>();
+
+            //Use Cases//
+            services.AddTransient<IViewSuppliersUseCase, ViewSuppliersUseCase>();
+            services.AddTransient<IAddSupplierUseCase, AddSupplierUseCase>();
+           
+
+            return services;
+        }
+    }
+}
